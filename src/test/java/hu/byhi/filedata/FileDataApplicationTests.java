aa819a4fd997c6eaeec7ccaa8759940b477b6b09
byhi <byhisoft@gmail.com>
2021-07-15 16:18:50 +0200

    init commit

---
+++ src/test/java/hu/byhi/filedata/FileDataApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.byhi.filedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
